f8f9a56bf9a26579ad8fa171c24c00590b4900d5
Venkat <venky541@gmail.com>
2012-11-09 14:30:43 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrillianceJobs::Application.config.secret_token = 'f2f3093316b98178027159bb553957ccb16c018a65a4b5b0b012fd1169f79b0e628555849f67383e67e36a7e6bc03b39f9f03742b575d6e78c61b180af29716d'
